4803b75a8e4fb97fcf05c6ff9e50e316b5c00c2c
Nicholas Vialpando <114171633+nvialp@users.noreply.github.com>
2022-12-30 01:38:54 +0000

    added variables in script file

---
+++ week 1/script.js
@@ -0,0 +1,6 @@
+let firstName = Nicholas;
+let lastName = Vialpando;
+yearsOfStudy = 7/12;
+goal = "finish my engineering degree";
+
+console.log("My name " + firstName + ", " + lastName + ". I have been at SCC for " + yearOfStudy + " and plan to " + goal + " after completing my courses");
