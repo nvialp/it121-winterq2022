6df4d84213661623f7f0190cc3771c3e15d16f8a
Nicholas Vialpando <114171633+nvialp@users.noreply.github.com>
2023-01-13 04:16:09 +0000

    cleaned up code, deleted console.log checks

--- week2/piglatin.js
+++ week2/piglatin.js
@@ -2,7 +2,7 @@ function isVowel(letter) {
   let vowels = ["a", "e", "i", "o", "u"];
   let vowel = false;
 
-  for (let i = 0; i <= vowels.length - 1; i++) {
+  for(let i = 0; i <= vowels.length - 1; i++) {
     if (vowels[i] == letter) {
       vowel = true;
     }
@@ -12,23 +12,26 @@ function isVowel(letter) {
 
 let newWord;
 let piglatin;
+let string;
 let translation = [];
 let word;
 
-let string = prompt("please enter word/phrase to be translated.");
 
-if (string.length ===0) {
+string = prompt("please enter word/phrase to be translated.");
+
+string = string.toLowerCase();
+
+if(string.length ===0) {
   piglatin = "Error, nothing entered";
 }
 
 else {
   sArray = string.split(" ");
-  console.log(sArray.length);
-  console.log(sArray);
-  if (sArray.length == 1) {
+    
+  if(sArray.length == 1) {
     word = sArray[0];
   
-    if (isVowel(word[0])) {
+    if(isVowel(word[0])) {
       newWord = word + "way";
     }
   
@@ -44,13 +47,13 @@ else {
     piglatin = newWord;
   }
   
-  else if (sArray.length > 1){
+  else if(sArray.length > 1) {
   
-    for (let i = 0; i <= sArray.length - 1; i++) {
+    for(let i = 0; i <= sArray.length - 1; i++) {
   
       let temp = sArray[i];
   
-      if (isVowel(temp[0])) {
+      if(isVowel(temp[0])) {
         newWord = temp + "way";
       }
   
@@ -58,16 +61,16 @@ else {
         if (isVowel(temp[1])) {
           newWord = temp.substring(1) + temp.substring(0, 1) + "ay";
         }
+            
         else {
           newWord = temp.substring(2) + temp.substring(0, 2) + "ay";
         }
       }
-      //word ='';
+        
       translation[i] = newWord;
     }
   
-    piglatin = translation.join(" ") + ".";
-    
+    piglatin = translation.join(" ") + ".";    
   }
 }
 
