660f2b70859455eee75f8f36b1998c67a8ae9913
Nicholas Vialpando <114171633+nvialp@users.noreply.github.com>
2023-01-19 05:55:08 +0000

    updated href for style sheet, and fixed week one assignment

--- week1/script.js
+++ week1/script.js
@@ -3,4 +3,6 @@ let lastName = "Vialpando";
 let yearsOfStudy = 0.6;
 let goal = "finish my engineering degree";
 
-document.write("My name " + firstName + ", " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
+let aboutMe ="My name " + firstName + ", " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.";
+
+document.getElementById("aboutme").innerHTML = aboutMe;
