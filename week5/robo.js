712b18aeac38f46be5eaec41757030d5a94b2ebb
Nicholas Vialpando <114171633+nvialp@users.noreply.github.com>
2023-02-03 02:32:20 +0000

    completed rob dealer for week 5

--- week5/robo.js
+++ week5/robo.js
@@ -1,8 +1,10 @@
-let deck = [];
+
 let hand = [];
 let suit = ["C", "D", "H", "S"];
 let value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
+        
+let deck = new Array();
+        
 for (let i = 0; i < value.length; i++) {
     for (let j = 0; j < suit.length; j++) {
         let card = {Value: value[i], Suit: suit[j]};
@@ -12,15 +14,23 @@ for (let i = 0; i < value.length; i++) {
 
 
 function deal(deck) {
-    for (let i = 0; i < 5; i++) {
-        hand[i] = deck.splice(Math.floor(Math.random() * deck.length), 1);
+    //deck = getDeck();
+
+    while(deck.length != 0) {
+        if (deck.length > 5)
+            for (let i = 0; i < 5; i++) {
+                hand[i] = deck.splice(Math.floor(Math.random() * deck.length), 1);
+            }
+    
+        else if(deck.length < 6) {
+            for (let i = 0; i < deck.length; i++) {
+                hand[i] = deck.splice(Math.floor(Math.random() * deck.length), 1);
+            }
+        }
+    
+        return hand;
     }
-
-    return hand;
 }
-
-
-hand = deal(deck);
 
 
 https://www.thatsoftwaredude.com/content/6196/coding-a-card-deck-in-javascript
